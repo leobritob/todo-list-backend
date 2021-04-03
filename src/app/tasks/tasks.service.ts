4b8536b85f3b973ac7eeb6a3052196ce309f22a3
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-03 18:03:59 -0300

    feat(tasks): create service

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TasksEntity } from './tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(TasksEntity)
+    private readonly tasksRepository: Repository<TasksEntity>,
+  ) {}
+
+  async index() {
+    return await this.tasksRepository.find();
+  }
+
+  async store(data) {
+    return await this.tasksRepository.save(this.tasksRepository.create(data));
+  }
+
+  async show(id: string) {
+    return await this.findByIdOrFail(id);
+  }
+
+  async update(id: string, data) {
+    const user = await this.tasksRepository.findOneOrFail(id);
+
+    this.tasksRepository.merge(user, data);
+    return await this.tasksRepository.save(user);
+  }
+
+  async destroy(id: string) {
+    await this.findByIdOrFail(id);
+
+    await this.tasksRepository.softDelete({ id });
+  }
+
+  private async findByIdOrFail(id: string) {
+    try {
+      return await this.tasksRepository.findOneOrFail(id);
+    } catch (e) {
+      throw new NotFoundException(e.message);
+    }
+  }
+}
