a9d1362c5e42cc7ea190bc7bb43c1dd82877d835
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-03 17:23:26 -0300

    chore: configure project dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<any>('TYPEORM_CONNECTION'),
+        host: configService.get<string>('TYPEORM_HOST'),
+        database: configService.get<string>('TYPEORM_DATABASE'),
+        username: configService.get<string>('TYPEORM_USERNAME'),
+        password: configService.get<string>('TYPEORM_PASSWORD'),
+        port: Number(configService.get<number>('TYPEORM_PORT', 3306)),
+        entities: [__dirname + '/**/*.entity.js'],
+        synchronize:
+          configService.get('TYPEORM_SYNCHRONIZE', 'false') === 'true',
+        logging: 'all',
+      }),
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('TODO List Services')
+    .setDescription('The TODO List Services API')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.HTTP_PORT || 3000);
 }
 bootstrap();
