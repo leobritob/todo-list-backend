ba9d4d9dd6659941c44aac5a7da7e8802a254d2e
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-03 18:05:21 -0300

    feat(tasks): create controller

--- src/app/tasks/tasks.controller.ts
+++ src/app/tasks/tasks.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller('api/v1/tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  async index() {
+    return await this.tasksService.index();
+  }
+
+  @Post()
+  async store(@Body() body) {
+    return await this.tasksService.store(body);
+  }
+
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.tasksService.show(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.tasksService.update(id, body);
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.tasksService.destroy(id);
+  }
+}
