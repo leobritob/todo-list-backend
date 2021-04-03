bfe8eb591a8a3175e14a767a8fdf3251c8cc83ba
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-03 17:55:34 -0300

    feat(projects): create controller

--- src/app/projects/projects.controller.ts
+++ src/app/projects/projects.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProjectsService } from './projects.service';
 
 @Controller('api/v1/projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Get()
+  async index() {
+    return await this.projectsService.index();
+  }
+
+  @Post()
+  async store(@Body() body) {
+    return await this.projectsService.store(body);
+  }
+
+  @Get(':id')
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.projectsService.show(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+    return await this.projectsService.update(id, body);
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.projectsService.destroy(id);
+  }
+}
