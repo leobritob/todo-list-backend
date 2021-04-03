f449087850fb2f7705d497adc241c27056799863
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-03 17:40:59 -0300

    feat(users): create service

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersEntity } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  async index() {
+    return await this.usersRepository.find();
+  }
+
+  async store(data) {
+    return await this.usersRepository.save(this.usersRepository.create(data));
+  }
+
+  async show(id: string) {
+    return await this.findByIdOrFail(id);
+  }
+
+  async update(id: string, data) {
+    const user = await this.usersRepository.findOneOrFail(id);
+
+    this.usersRepository.merge(user, data);
+    return await this.usersRepository.save(user);
+  }
+
+  async destroy(id: string) {
+    await this.findByIdOrFail(id);
+
+    await this.usersRepository.softDelete({ id });
+  }
+
+  private async findByIdOrFail(id: string) {
+    try {
+      return await this.usersRepository.findOneOrFail(id);
+    } catch (e) {
+      throw new NotFoundException(e.message);
+    }
+  }
+}
